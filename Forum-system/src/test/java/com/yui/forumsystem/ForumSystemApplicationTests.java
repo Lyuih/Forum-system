4473f9fdced4374f0b09a8d6ea44292a977d2367
jieyijieyi <642948984@qq.com>
2025-10-04 23:40:41 +0800

    Maven跑通,test运行成功

---
+++ Forum-system/src/test/java/com/yui/forumsystem/ForumSystemApplicationTests.java
@@ -0,0 +1,14 @@
+package com.yui.forumsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("you can see me");
+    }
+
+}
---
+++ Forum-system/src/test/java/com/yui/forumsystem/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.yui.forumsystem.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+// 指定测试根路径映射地址前缀
+
+// Controller注解，返回的对象⽤JSON形式表⽰
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Spring Boot...";
+    }
+}
