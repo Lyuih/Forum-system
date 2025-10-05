99d9899d0e9df159ac3d5f2c0c305845bb2cff2d
jieyijieyi <642948984@qq.com>
2025-10-05 12:40:17 +0800

    修改controller目录地址，访问页面成功

--- Forum-system/src/main/java/com/yui/forumsystem/controller/TestController.java
+++ Forum-system/src/main/java/com/yui/forumsystem/controller/TestController.java
@@ -1,11 +1,13 @@
 package com.yui.forumsystem.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 // 指定测试根路径映射地址前缀
 
 // Controller注解，返回的对象⽤JSON形式表⽰
 @RestController
+@RequestMapping("/test")
 public class TestController {
     @GetMapping("/hello")
     public String hello() {
